ac33d2b4ca6c47552fdbf43769d027c93bca70ba
hashimura-masaki <motimoti0730@gmail.com>
2025-04-06 18:41:27 +0900

    いったん作成してみる

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('one-button-toggle-case.toggleCase', async() => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('No active editor');
+			return;
+		}
+		const document = editor.document;
+		const selections = editor.selections;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "one-button-toggle-case" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('one-button-toggle-case.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from one-button-toggle-case!');
+		await editor.edit((editBuilder) => {
+			selections.forEach((selection) => {
+				const text = document.getText(selection);
+				if(text.indexOf(' ') !== -1) {
+					vscode.window.showInformationMessage('Invalid text:' + text);
+				}
+				const toggleText = toggleCase(text);
+				editBuilder.replace(selection, toggleText);
+			});
+		});
 	});
-
 	context.subscriptions.push(disposable);
+}
+
+function toggleCase(text: string): string {
+	
+	return "1";
 }
 
 // This method is called when your extension is deactivated
